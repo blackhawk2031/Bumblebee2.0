e620c16cbef99bd7e1e443b8ecf6fcc06c420d94
blackhawk2031 <janapraveen31@gmail.com>
2025-01-08 14:34:16 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -389,16 +389,16 @@ async def get_shortlink(link):
               'format': 'json'
               }
 
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                data = await response.json(content_type='text/html')
-                if data["status"] == "success":
-                    return data['shortlink']
-                else:
-                    logger.error(f"Error: {data['message']}")
-                    return f'https://tnlink.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+    #try:
+        #async with aiohttp.ClientSession() as session:
+            #async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                #data = await response.json(content_type='text/html')
+                #if data["status"] == "success":
+                    #return data['shortlink']
+                #else:
+                    #logger.error(f"Error: {data['message']}")
+                    #return f'https://tnlink.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
 
-    except Exception as e:
-        logger.error(e)
-        return f'https://tnlink.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+    #except Exception as e:
+        #logger.error(e)
+        #return f'https://tnlink.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
